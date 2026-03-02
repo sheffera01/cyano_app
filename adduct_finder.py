fea68470519d50cd5b43c3dd551f9b6b8f4b9422
sheffera01 <sheffera@umich.edu>
2026-03-02 12:28:58 -0500

    Update adduct_finder.py charge state must be same

--- adduct_finder.py
+++ adduct_finder.py
@@ -114,7 +114,13 @@ def detect_adducts(
     for i in range(len(d)):
         for j in range(i + 1, len(d)):
             dmz = mz_vals[j] - mz_vals[i]
+
             z_i = z_vals[i] if z_vals[i] else cfg.max_charge_assumed
+            z_j = z_vals[j] if z_vals[j] else cfg.max_charge_assumed
+
+            # Enforce same-charge linking
+            if z_i != z_j:
+                continue
 
             for name, dmass in cfg.deltas.items():
                 exp_dmz = _expected_dmz(dmass, z_i)
